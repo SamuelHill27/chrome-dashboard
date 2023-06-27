25c4ec44dfed8bb1f6f40c44ee17892a535d2751
SamuelHill27 <samuelshill04@gmail.com>
2023-06-27 16:08:21 +0100

    Complete TimeDate functionality and basic styling

--- src/components/TimeDate.js
+++ src/components/TimeDate.js
@@ -1,14 +1,27 @@
 import styles from "./TimeDate.module.css";
+import { useEffect, useState } from "react";
 
 const TimeDate = () => {
-  const [time, setTime] = useState();
+  const [date, setDate] = useState(new Date());
 
-  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [date]);
 
   return (
     <section className={styles.container}>
-      <div className={`${styles.time} ${styles.container}`}>Time</div>
-      <div className={`${styles.date} ${styles.container}`}>Date</div>
+      <div className={styles.time}>{date.toLocaleTimeString()}</div>
+      <div className={styles.date}>
+        {date.toLocaleDateString("en-GB", {
+          weekday: "long",
+          day: "numeric",
+          month: "long",
+        })}
+      </div>
     </section>
   );
 };
